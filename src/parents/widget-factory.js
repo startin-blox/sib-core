be10d0a56a508f0f5c6fbf55c253c204a34ab3cc
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2019-02-25 16:34:43 +0700

    syntax: improve readability of code

--- src/parents/widget-factory.js
+++ src/parents/widget-factory.js
@@ -22,7 +22,7 @@ export const widgetFactory = (customTemplate) => class extends HTMLElement {
     this.render();
   }
   get value() {
-    return (this.dataHolder && JSON.stringify(this.dataHolder.value) != JSON.stringify({})) // if dataHolder is not an empty object
+    return (this.dataHolder && JSON.stringify(this.dataHolder.value) != "{}") // if dataHolder is not an empty object
       ? this.dataHolder.value
       : this._value || ''
   }
