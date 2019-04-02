fcef4166b29bb7f19d0be0476157233fcb688885
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2019-04-02 16:05:24 +0700

    bugfix: allow to search boolean

--- src/mixins/sib-list-mixin.js
+++ src/mixins/sib-list-mixin.js
@@ -41,8 +41,8 @@ const SIBListMixin = superclass =>
           propertyValue['@id'] === filterValue['@id']
         );
       }
-      if (typeof propertyValue === 'number') {
-        //check if integer match
+      if (typeof propertyValue === 'number' || typeof propertyValue === 'boolean') {
+        //check if integer or boolean match
         return propertyValue === filterValue;
       }
       if (typeof propertyValue === 'string') {
@@ -54,6 +54,7 @@ const SIBListMixin = superclass =>
       return false;
     }
 
+    // TODO : to be moved in the store and mutualized with widgetMixin.getValue
     applyFilterToResource(resource, filter) {
       if (!Array.isArray(filter)) return resource[filter];
       if (filter.length === 0) return;
