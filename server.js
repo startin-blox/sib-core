36fba0a2dc1b3709110c5b33ab556ef0cb3be662
Clément <cpartiot@gmail.com>
2019-05-02 15:57:42 +0200

    update: server find free port

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-//const port = 8080;
 const distPath = '.';
 
 // express server
 const { join } = require('path');
 const express = require('express');
+const port = require('find-free-port')(3000);
 const app = express();
-const server = app
-  .use(express.static(distPath, ))
-  // .use('/src', express.static(join(__dirname, 'src')))
-  .get(/^[^.]*$/, (req, rep) =>
-    rep.sendFile(join(__dirname, distPath, '/index.html')),
-  )
-  .listen();
-console.log(address(server.address()));
-function address(address){
-  if(address.family === 'IPv6') address.address = `[${address.address}]`;
-  return `http://${address.address}:${address.port}`
-}
+(async ()=>{
+  const server = app
+    .use(express.static(distPath))
+    // .use('/src', express.static(join(__dirname, 'src')))
+    .get(/^[^.]*$/, (req, rep) =>
+      rep.sendFile(join(__dirname, distPath, '/index.html')),
+    )
+    .listen((await port)[0]);
+  console.log(address(server.address()));
+  function address(address) {
+    if (address.family === 'IPv6') address.address = `[${address.address}]`;
+    return `http://${address.address}:${address.port}`;
+  }
+})();
