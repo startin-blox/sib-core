6a849379a157551c2b291bdf6b1acc695b6daa9d
Clément <cpartiot@gmail.com>
2019-05-02 13:08:29 +0200

    update: server use random port

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-const port = 8080;
+//const port = 8080;
 const distPath = '.';
 
 // express server
 const { join } = require('path');
 const express = require('express');
 const app = express();
-app
+const server = app
   .use(express.static(distPath, ))
   // .use('/src', express.static(join(__dirname, 'src')))
   .get(/^[^.]*$/, (req, rep) =>
     rep.sendFile(join(__dirname, distPath, '/index.html')),
   )
-  .listen(port);
-console.log(`http://localhost:${port}`);
+  .listen();
+console.log(address(server.address()));
+function address(address){
+  if(address.family === 'IPv6') address.address = `[${address.address}]`;
+  return `http://${address.address}:${address.port}`
+}
