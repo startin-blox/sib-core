1bb120ba901b032ed5304e231b53dd1f10ca478d
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2019-07-31 14:49:44 +0200

    fitWorld instead of center on Europe

--- src/components/sib-map.ts
+++ src/components/sib-map.ts
@@ -36,12 +36,13 @@ export const SibMap = {
     }
   },
   reset() {
-    const bounds = this.markers.length ?
-      L.featureGroup(this.markers).getBounds() : // get bounds from markers
-      [[38.7077, -9.1365], [56.8245, 60.6072]]; // default bounds if no marker (Europe)
-
     this.map.invalidateSize();
-    this.map.fitBounds(bounds);
+
+    if (this.markers.length) {
+      this.map.fitBounds(L.featureGroup(this.markers).getBounds()); // Center map on markers if some available
+    } else {
+      this.map.fitWorld(); // ... or on the world if not
+    }
   },
   dispatchSelect(event: CustomEvent): void {
     const target = event.target as Element;
