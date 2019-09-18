43386d1db6fa23c1b1d2222126eb2b0b42078cb7
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2019-09-18 16:51:33 +0200

    fix sib-map

--- src/components/sib-map.ts
+++ src/components/sib-map.ts
@@ -1,8 +1,9 @@
 import { Sib } from '../libs/Sib.js';
 import { ListMixin } from '../mixins/listMixin.js';
 import { StoreMixin } from '../mixins/storeMixin.js';
-import { LocationResourceInterface } from '../libs/interfaces.js';
 import { importCSS } from '../libs/helpers.js';
+import { store } from '../libs/store/store.js';
+
 //@ts-ignore
 import L from 'https://dev.jspm.io/leaflet';
 
@@ -56,11 +57,17 @@ export const SibMap = {
       );
     }
   },
-  appendChildElt(resource: LocationResourceInterface): void {
-    if (resource.lat && resource.lng) {
-      const marker = L.marker([resource.lat, resource.lng], {
+  async appendChildElt(resourceId: string) {
+    await store.initGraph(resourceId, this.context);
+    const resource = store.get(resourceId);
+    const lat = await resource['lat'];
+    const lng = await resource['lng'];
+
+    if (lat && lng) {
+      const marker = L.marker([lat.toString(), lng.toString()], {
         resource: resource,
       });
+
       marker.addTo(this.map).on('click', this.dispatchSelect.bind(this));
       this.markers.push(marker);
     }
@@ -72,10 +79,14 @@ export const SibMap = {
   isSet() {
     return false;
   },
-  populate(): void {
-    for (let resource of this.resources) this.appendChildElt(resource);
+  async renderDOM(resources: object[], listPostProcessors: Function[], div: HTMLElement, context: string) {
+    for await (let resource of resources) await this.appendChildElt(resource['@id']);
+
     this.reset();
-  },
+
+    const nextProcessor = listPostProcessors.shift();
+    if(nextProcessor) await nextProcessor(resources, listPostProcessors, div, context);
+  }
 };
 
 Sib.register(SibMap);
