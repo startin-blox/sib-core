f620b2314f21ea3b9f95d805e11540d63c0fc955
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2019-10-17 16:14:30 +0200

    bugfix: set a context for multiple select children widgets

--- src/mixins/widgetMixin.ts
+++ src/mixins/widgetMixin.ts
@@ -115,6 +115,7 @@ const WidgetMixin = {
   widgetAttributes(field: string): object {
     const attrs = {
       name: field,
+      context: this.context
     };
     const escapedField = this.getEscapedField(field);
     for (let attr of ['range', 'label', 'class']) {
@@ -136,7 +137,6 @@ const WidgetMixin = {
     }
     if (this.getAction(escapedField) && this.resource) attrs['src'] = this.resource['@id'];
     attrs['resourceId'] = this.resource ? this.resource['@id'] : null;
-    attrs['context'] = this.context;
 
     return attrs;
   },
