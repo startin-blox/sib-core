5839bd9318d7fc9dfffc339c581285723fbc6a52
Clément <cpartiot@gmail.com>
2019-11-13 17:26:14 +0100

    update: add cypress in scripts

--- cypress/integration/solid-form-file.js
+++ cypress/integration/solid-form-file.js
@@ -1,12 +1,12 @@
 describe('solid-form-file test', function() {
-  it('test solid-form-file', function() {
+  it('upload file with solid-form-file', function() {
     cy.visit('http://0.0.0.0:3000/examples/solid-form-file.html')
     cy.get('[name=picture]+input[type=file]').uploadFile('../../fake-image.svg')
     cy.get('input[name=picture]').should($i => {
       expect($i.val()).to.match(/\/upload\/[0-9a-f]+.jpg$/)
     })
   })
-  it('test solid-form-file-image', function() {
+  it('upload image with solid-form-file-image', function() {
     cy.visit('http://0.0.0.0:3000/examples/solid-form-file.html')
     cy.get('solid-form-file-image input[type=file]').uploadFile('../../fake-image.svg')
     cy.get('solid-form-file-image img').should($i => {
