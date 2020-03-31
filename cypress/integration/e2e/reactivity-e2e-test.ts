7e3e7032e2bfe9463c0c0d9cfe87a2cccff2e53a
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2020-03-31 14:50:36 +0200

    add reactivity test

--- cypress/integration/e2e/reactivity-e2e-test.ts
+++ cypress/integration/e2e/reactivity-e2e-test.ts
@@ -1,6 +1,7 @@
-describe('simple Startin’blox e2e test', function() {
+
+describe('simple Startin’blox e2e test', function () {
   this.beforeAll('visit', () => {
-    cy.visit('/examples/e2e/reactivity-e2e-test.html')
+    cy.visit('/examples/e2e/reactivity-e2e-test.html');
   })
   it('check display', () => {
     cy.get('body > solid-display > div > solid-display-value:nth-child(1)').should('have.text', 'Test');
@@ -8,6 +9,27 @@ describe('simple Startin’blox e2e test', function() {
     cy.get('body > solid-display > div > solid-display-value:nth-child(3)').should('have.text', 'admin');
   });
   it('checks reactivity', () => {
-    // check updates in DOM
+    cy.server();
+    cy.route({ // on PUT, override headers
+      url: '**/user-1.jsonld',
+      method: 'PUT',
+      headers: { 'content-type': 'application/ld+json' },
+      onRequest: (xhr) => { xhr.setRequestHeader('content-type', 'application/ld+json') }
+    })
+
+    // type data
+    cy.get('body > solid-form input[name="first_name"]').clear().type('Monsieur');
+    cy.get('body > solid-form input[name="last_name"]').clear().type('Administrateur');
+    cy.get('body > solid-form input[name="username"]').clear().type('superadmin');
+
+    // stub response for GET /user-1.jsonld
+    cy.route('GET', '**/user-1.jsonld', 'fixture:user-1-edited.json')
+
+    cy.get('body > solid-form input[type=submit]').click();
+
+    // check sib-display update
+    cy.get('body > solid-display > div > solid-display-value:nth-child(1)').should('have.text', 'Monsieur');
+    cy.get('body > solid-display > div > solid-display-value:nth-child(2)').should('have.text', 'Administrateur');
+    cy.get('body > solid-display > div > solid-display-value:nth-child(3)').should('have.text', 'superadmin');
   });
 })
