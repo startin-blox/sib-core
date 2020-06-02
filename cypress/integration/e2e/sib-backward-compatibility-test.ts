296fff578e74039e4942cb1f1586e19872521bf3
Clément <cpartiot@gmail.com>
2020-06-02 12:32:46 +0200

    improve backward compatibility test

--- cypress/integration/e2e/sib-backward-compatibility-test.ts
+++ cypress/integration/e2e/sib-backward-compatibility-test.ts
@@ -2,34 +2,26 @@ describe('Backward Compatibility of sib elements test', function () {
   this.beforeAll('visit', () => {
     cy.visit('/examples/e2e/sib-backward-compatibility.html');
   });
-  it('check solid-form children type', () => {
-    cy.get('solid-form').should('have.descendants', 'solid-form-label-text');
-    cy.get('solid-form').should('not.have.descendants', 'sib-form-label-text');
-  });
+  it('check sub tags', () => {
+    cy.get('#solid-form, #sib-form, #solid-display, #sib-display').then(elms => {
+      elms.each((_, elm) => {
+        const isSib = elm.localName.startsWith('sib-');
+        elm.addEventListener('populate',() => {
+          const childrenTags = [...new Set(elm.querySelectorAll("*"))]
+            .map((elm) => elm.localName)
+            .filter((tag) => tag.includes('-'));
+          const solidChildren = childrenTags.filter(tag => tag.startsWith('solid-'));
+          const sibChildren = childrenTags.filter(tag => tag.startsWith('sib-'));
 
-  it('check sib-form children type', () => {
-    cy.get('sib-form').should('have.descendants', 'sib-form-label-text');
-    cy.get('sib-form').should('not.have.descendants', 'solid-form-label-text');
-  });
-  it('check solid-display children type', () => {
-    cy.get('solid-display').should('have.descendants', 'solid-display');
-    cy.get('solid-display').should('have.descendants', 'solid-set-default');
-    cy.get('solid-display').should('have.descendants', 'solid-display-value');
-    cy.get('solid-display').should('have.descendants', 'solid-form');
-    cy.get('solid-display').should('not.have.descendants', 'sib-display');
-    cy.get('solid-display').should('not.have.descendants', 'sib-set-default');
-    cy.get('solid-display').should('not.have.descendants', 'sib-display-value');
-    cy.get('solid-display').should('not.have.descendants', 'sib-form');
-  });
-
-  it('check sib-display children type', () => {
-    cy.get('sib-display').should('have.descendants', 'sib-display');
-    cy.get('sib-display').should('have.descendants', 'sib-set-default');
-    cy.get('sib-display').should('have.descendants', 'sib-display-value');
-    cy.get('sib-display').should('have.descendants', 'sib-form');
-    cy.get('sib-display').should('not.have.descendants', 'solid-display');
-    cy.get('sib-display').should('not.have.descendants', 'solid-set-default');
-    cy.get('sib-display').should('not.have.descendants', 'solid-display-value');
-    cy.get('sib-display').should('not.have.descendants', 'solid-form');
+          if (isSib) {
+            expect(sibChildren).is.not.empty;
+            expect(solidChildren).is.empty;
+          } else {
+            expect(solidChildren).is.not.empty;
+            expect(sibChildren).is.empty;
+          }
+        })
+      });
+    })
   });
 });
