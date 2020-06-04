66d5ad76083db7adbcf7a30dca3300447871bb77
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2020-06-04 14:02:12 +0200

    update: fix order by test #632

--- cypress/integration/e2e/order-by.spec.ts
+++ cypress/integration/e2e/order-by.spec.ts
@@ -9,31 +9,62 @@ describe('order-by', function() {
     cy.get('#list-1 > div').as('list');
 
     cy.get('@list').children()
-    .should('have.length', 4);
+    .should('have.length', 8);
     // Check elements order
     cy.get('@list').find('solid-display').eq(0)
-    .should('have.attr', 'data-src', 'user-1.jsonld')
+    .should('have.attr', 'data-src', 'skill-2.jsonld')
     .within(() => {
-      cy.get('solid-display-value[name="@id"]').contains('user-1.jsonld');
-      cy.get('solid-display-value[name="username"]').contains('admin');
+      cy.get('solid-display-value[name="@id"]').contains('skill-2.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('CSS');
     });
+
     cy.get('@list').find('solid-display').eq(1)
-    .should('have.attr', 'data-src', 'user-3.jsonld')
+    .should('have.attr', 'data-src', 'skill-4.jsonld')
     .within(() => {
-      cy.get('solid-display-value[name="@id"]').contains('user-3.jsonld');
-      cy.get('solid-display-value[name="username"]').contains('not-member-paris');
+      cy.get('solid-display-value[name="@id"]').contains('skill-4.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('DevOps');
     });
+
     cy.get('@list').find('solid-display').eq(2)
-    .should('have.attr', 'data-src', 'user-2.jsonld')
+    .should('have.attr', 'data-src', 'skill-6.jsonld')
     .within(() => {
-      cy.get('solid-display-value[name="@id"]').contains('user-2.jsonld');
-      cy.get('solid-display-value[name="username"]').contains('paris');
+      cy.get('solid-display-value[name="@id"]').contains('skill-6.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('Git');
     });
+
     cy.get('@list').find('solid-display').eq(3)
-    .should('have.attr', 'data-src', 'user-4.jsonld')
+    .should('have.attr', 'data-src', 'skill-1.jsonld')
     .within(() => {
-      cy.get('solid-display-value[name="@id"]').contains('user-4.jsonld');
-      cy.get('solid-display-value[name="username"]').contains('pierre');
+      cy.get('solid-display-value[name="@id"]').contains('skill-1.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('HTML');
+    });
+
+    cy.get('@list').find('solid-display').eq(4)
+    .should('have.attr', 'data-src', 'skill-3.jsonld')
+    .within(() => {
+      cy.get('solid-display-value[name="@id"]').contains('skill-3.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('Javascript');
+    });
+
+    cy.get('@list').find('solid-display').eq(5)
+    .should('have.attr', 'data-src', 'skill-8.jsonld')
+    .within(() => {
+      cy.get('solid-display-value[name="@id"]').contains('skill-8.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('Node');
+    });
+
+    cy.get('@list').find('solid-display').eq(6)
+    .should('have.attr', 'data-src', 'skill-5.jsonld')
+    .within(() => {
+      cy.get('solid-display-value[name="@id"]').contains('skill-5.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('PHP');
+    });
+
+    cy.get('@list').find('solid-display').eq(7)
+    .should('have.attr', 'data-src', 'skill-7.jsonld')
+    .within(() => {
+      cy.get('solid-display-value[name="@id"]').contains('skill-7.jsonld');
+      cy.get('solid-display-value[name="name"]').contains('Python');
     });
   });
 
@@ -44,7 +75,7 @@ describe('order-by', function() {
     cy.get('#list-2').as('list');
 
     cy.get('@list').find('> div').children()
-    .should('have.length', 4);
+    .should('have.length', 8);
 
     cy.get('@list').find('> div > solid-display').then($div => {
       let currentOrder = Array.from($div.map((_index, el) => el.dataset.src))
