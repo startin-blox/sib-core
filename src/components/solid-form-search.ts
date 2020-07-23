126bd1176dfc14183a61e78a3f725a516c89c307
Clément <cpartiot@gmail.com>
2020-07-23 19:37:38 +0200

    update: remove useless solid-form-search parts

--- src/components/solid-form-search.ts
+++ src/components/solid-form-search.ts
@@ -45,34 +45,13 @@ export const SolidFormSearch = {
 
     return values;
   },
-  set value(value) {
-    this.widgets.forEach(el => {
-      try {
-        if(value[el.name]) el.value = value[el.name]
-      } catch (e) {}
-    });
-  },
-  get form(): Element {
-    if (this._form) return this._form;
-    return this.element;
-  },
-  get isNaked(): boolean {
-    return true;
-  },
-  async getFormValue() {
-    let value = this.value;
-    if (this.resource && !(await this.resource.isContainer())) value['@id'] = this.resourceId;
-    return value
-  },
   getWidget(field: string): WidgetInterface {
     let tagName = '';
     let type = WidgetType.CUSTOM;
     const widgetAttribute = this.element.getAttribute('widget-' + field);
 
     // Choose widget
-    if (!widgetAttribute && this.element.hasAttribute('upload-url-' + field)) {
-      tagName = 'solid-form-file'
-    } else if (!widgetAttribute && this.element.hasAttribute('range-' + field)) {
+    if (!widgetAttribute && this.element.hasAttribute('range-' + field)) {
       tagName = 'solid-form-dropdown'
     } else {
       tagName = widgetAttribute || this.defaultWidget;
@@ -93,88 +72,16 @@ export const SolidFormSearch = {
       }),
     );
   },
-  async save(): Promise<object> {
-    this.toggleLoaderHidden(false);
-    this.hideError();
-    const resource = await this.getFormValue();
-    resource['@context'] = this.context;
-    let saved;
-    try {
-      if (this.partial == null) {
-      saved = resource['@id'] ?
-        await store.put(resource, this.resourceId) :
-        await store.post(resource, this.resourceId);
-      } else {
-        saved = await store.patch(resource, this.resourceId);
-      }
-    } catch (e) {
-      this.toggleLoaderHidden(true);
-      if (e) { // if server error
-        this.showError(e);
-        throw e;
-      } // else, ldpframework error, we continue
-    }
-    this.element.dispatchEvent(
-      new CustomEvent('save', {
-        bubbles: true,
-        detail: {
-          resource: resource,
-          id: saved || null
-        },
-      }),
-    );
-    this.toggleLoaderHidden(true);
-    return saved;
-  },
-  async submitForm(): Promise<void> {
-    const isCreation = !('@id' in (await this.getFormValue()));
-    let id;
-    try {
-      id = await this.save() || this.getFormValue()['@id'];
-    } catch (e) { return }
-    if (isCreation && this.form !== this) this.reset(); // we reset the form only in creation mode
-    if (!this.next) return;
-    this.element.dispatchEvent(
-      new CustomEvent('requestNavigation', {
-        bubbles: true,
-        detail: { route: this.next, resource: {'@id': id} },
-      }),
-    );
-  },
   async inputChange(): Promise<void> {
-    this.change(await this.getFormValue());
-  },
-  createInput(type: string): HTMLInputElement {
-    const input = document.createElement('input');
-    input.type = type;
-    return input;
+    this.change(this.value);
   },
   empty(): void {
   },
-  showError(e: object) {
-    this.error = html`
-      <div data-id="error">
-        <p>${e}</p>
-      </div>
-    `;
-    this.populate();
-  },
-  hideError() {
-    const error = this.element.querySelector('[data-id=form-error]');
-    if (error) this.element.removeChild(error);
-  },
-  reset() {
-    this.element.querySelectorAll('select[multiple]').forEach((select: HTMLSelectElement) => { // reset multiple select
-      const options = select.querySelectorAll('option:checked') as NodeListOf<HTMLOptionElement>;
-      options.forEach(option => option.selected = false );
-      select.dispatchEvent(new Event('change'));
-    })
-  },
+  
   async populate(): Promise<void> {
     this.element.addEventListener('input', (event: Event) => this.inputChange(event));
     const fields = await this.getFields();
     const template = html`
-      ${this.error}
       <form>
         ${fields.map(field => this.createWidget(field))}
       </form>
