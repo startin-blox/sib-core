495ed53b29d21b0b0ace1d195e5756db0c62a20c
Matthieu Fesselier <matthieu.fesselier@gmail.com>
2020-07-30 11:27:13 +0200

    bugfix: autocomplete on creation

--- src/new-widgets/callbackMixins/autocompletionMixin.ts
+++ src/new-widgets/callbackMixins/autocompletionMixin.ts
@@ -16,14 +16,16 @@ const AutocompletionMixin = {
     if (this.slimSelect) return;
     let select = this.element.querySelector('select');
     if (select) {
-      this.slimSelect = new SlimSelect({ select });
-      select.addEventListener('change', () => this.slimSelect.render());
-
-      // when data changes, re-build slimSelect
-      new MutationObserver(() => {
-        this.slimSelect.destroy();
-        this.slimSelect = new SlimSelect({ select });
-      }).observe(select, {
+      this.initSlimSelect(select);
+    } else {
+      // if no select available, wait for one and init slimSelect
+      new MutationObserver((_m, observer) => {
+        const select = this.element.querySelector('select');
+        if (select) {
+          this.initSlimSelect(select);
+          observer.disconnect(); // then disconnect mutationObserver
+        }
+      }).observe(this.element, {
         childList: true,
         characterData: true,
         subtree: true,
@@ -31,6 +33,21 @@ const AutocompletionMixin = {
     }
     const nextProcessor = listCallbacks.shift();
     if (nextProcessor) nextProcessor(value, listCallbacks);
+  },
+  initSlimSelect(select: Node) {
+    this.slimSelect = new SlimSelect({ select });
+    select.addEventListener('change', () => this.slimSelect.render());
+
+    // when data changes, re-build slimSelect
+    new MutationObserver(() => {
+      this.slimSelect.destroy();
+      this.slimSelect = new SlimSelect({ select });
+    }).observe(select, {
+      childList: true,
+      characterData: true,
+      subtree: true,
+    });
+
   }
 }
 
