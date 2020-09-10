c1aa670b6161a658dc5dc73bd511729533814e34
Clément <cpartiot@gmail.com>
2020-09-10 08:48:41 +0200

    update: add tests for fuzzy search

--- cypress/integration/e2e/filteredBy.spec.ts
+++ cypress/integration/e2e/filteredBy.spec.ts
@@ -1,13 +1,13 @@
 describe('simple Startin’blox e2e test', function() {
   it('check children count', () => {
     cy.visit('/examples/filtered.html')
-    cy.get('#filter1 input').type('hilton')
-    cy.get('#filter2 input').type('paris')
+    cy.get('#filter1 input').type('ma#er') // match "mayer"
+    cy.get('#filter2 input').type('linds') // match "lindsay", "collins" & "rollins"
     cy.get('main > div:nth-child(1) > solid-display > div > solid-display').its('length').should('eq', 1);
     cy.get('main > div:nth-child(2) > solid-display > div > solid-display').its('length').should('eq', 1);
-    cy.get('#filter1 input').clear().type('-')
-    cy.get('main > div:nth-child(1) > solid-display > div > solid-display').its('length').should('eq', 3);
+    cy.get('#filter1 input').clear().type('john') // match "johnston.ashley" & "frye.johns"
+    cy.get('main > div:nth-child(1) > solid-display > div > solid-display').its('length').should('eq', 2);
     cy.get('main > div:nth-child(2) > form input[value="filter2"]').click()
-    cy.get('main > div:nth-child(2) > solid-display > div > solid-display').its('length').should('eq', 2);
+    cy.get('main > div:nth-child(2) > solid-display > div > solid-display').its('length').should('eq', 3);
   })
 })
