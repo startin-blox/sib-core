c82c46441d6df07cce1774a045f11955a076a222
Clément <cpartiot@gmail.com>
2020-11-13 15:58:21 +0100

    update: add checkboxes widget

---
+++ src/new-widgets/templatesDependencies/formCheckboxesMixin.ts
@@ -0,0 +1,36 @@
+const FormCheckboxesMixin = {
+  name: 'form-checkboxes-mixin',
+  attributes: {
+    values: {
+      type: String,
+      default: '',
+      callback: function (value: string) {
+        if (!value) return;
+        try {
+          this.listAttributes['values'] = JSON.parse(value);
+        } catch (e) {
+          console.error(e);
+          this.listAttributes['values'] = [];
+        }
+        this.render();
+        this.dispatchEvent(new Event('change'));
+      }
+    },
+  },
+  created() {
+    this.listAttributes['values'] = [];
+  },
+  getValue() {
+    const options = Array.from(this.element.querySelectorAll('input')) as HTMLInputElement[];
+    return options
+      .filter(el => el.checked)
+      .map(el => el.value ? JSON.parse(el.value) : null)
+  },
+  get multiple() {
+    return true;
+  }
+}
+
+export {
+  FormCheckboxesMixin
+}
