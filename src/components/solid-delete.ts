e4c8309b2a419dd5ae7a8f2c445ec539cc9a6f76
Manon Bourgognon <manon.bourgognon@epitech.eu>
2021-02-12 16:36:57 +0100

    feature: create dialog tag, first draft

--- src/components/solid-delete.ts
+++ src/components/solid-delete.ts
@@ -1,6 +1,7 @@
 import { Sib } from '../libs/Sib';
 import { base_context, store } from '../libs/store/store';
 import { NextMixin } from '../mixins/nextMixin';
+import { uniqID } from '../libs/helpers';
 
 import { html, render } from 'lit-html';
 
@@ -27,6 +28,10 @@ export const SolidDelete = {
       type: String,
       default: null
     },
+    modalDialog: {
+      type: String,
+      default: null
+    }
   },
   created(): void {
     this.render();
@@ -45,18 +50,47 @@ export const SolidDelete = {
     e.stopPropagation();
     if (!this.dataSrc) return;
     if ((!this.confirmationMessage) || (this.confirmationMessage && confirm(this.confirmationMessage))) {
-      return store.delete(this.dataSrc, this.context).then(response => {
-        if (!response.ok) return;
-        this.goToNext(null);
-        const eventData = { detail: { resource: { "@id": this.dataSrc } }, bubbles: true };
-        this.element.dispatchEvent(new CustomEvent('save', eventData));
-        this.element.dispatchEvent(new CustomEvent('resourceDeleted', eventData)); // Deprecated. To remove in 0.15
-      });
+      if (this.modalDialog) {
+        var dialog : any = document.getElementById(this.dialogID);
+        dialog.showModal();
+      }
+      else this.deletion();
     };
+  },
+  deletion() {
+    return store.delete(this.dataSrc, this.context).then(response => {
+      if (!response.ok) return;
+      this.goToNext(null);
+      const eventData = { detail: { resource: { "@id": this.dataSrc } }, bubbles: true };
+      this.element.dispatchEvent(new CustomEvent('save', eventData));
+      this.element.dispatchEvent(new CustomEvent('resourceDeleted', eventData)); // Deprecated. To remove in 0.15
+    })
+  },
+  getModalDialog() {
+    this.dialogID = uniqID();
+    const quitDialog = () => {
+      var dialog : any = document.getElementById(this.dialogID);
+      if(dialog == null) return;
+      dialog.removeAttribute('open');
+    }
+    const deletion = () =>  {
+      this.deletion();
+      quitDialog();
+    }
+    return html`
+      <dialog id="${this.dialogID}">
+        <p>${this.modalDialog}</p>
+        <div>
+          <button @click=${deletion}>Yes</button>
+          <button @click=${quitDialog}>Cancel</button>
+        </div>
+      </dialog>
+      `
   },
   render(): void {
     const button = html`
       <button @click=${this.delete.bind(this)}>${this.dataLabel}</button>
+      ${this.modalDialog ? this.getModalDialog() : ''}
     `;
     render(button, this.element);
   }
