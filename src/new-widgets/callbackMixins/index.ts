27e81693aab71e23192bd55dcf84573c886491f3
Manon Bourgognon <manon.bourgognon@epitech.eu>
2022-09-13 19:02:09 +0200

    update: delete editormixin import

--- src/new-widgets/callbackMixins/index.ts
+++ src/new-widgets/callbackMixins/index.ts
@@ -1,16 +1,13 @@
 import { AutocompletionMixin } from './autocompletionMixin';
 import { RichtextMixin } from './richtextMixin';
-import { EditorMixin } from './editorMixin';
 
 const callbackDirectory = {
   autocompletion: AutocompletionMixin,
   richtext: RichtextMixin,
-  editor: EditorMixin
 }
 
 export {
   callbackDirectory,
   AutocompletionMixin,
   RichtextMixin,
-  EditorMixin
 }
