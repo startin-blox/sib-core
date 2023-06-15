50c88b930ee9e9b6fd75a8f01528d103a55e3c27
Benoit Alessandroni <benoit@happy-dev.fr>
2023-06-15 19:46:47 +0200

    clean: removing logs

--- src/libs/store/server-pagination.ts
+++ src/libs/store/server-pagination.ts
@@ -6,13 +6,10 @@ export interface ServerPaginationOptions {
 export function formatAttributesToServerPaginationOptions(
     elementAttributes: { name: string, value: number }[]
 ): ServerPaginationOptions | undefined {
-    console.log(elementAttributes);
     const attributes = new Map(Array.from(elementAttributes).map(({ name, value }) => [name, value]))
-    console.log(attributes);
     const limit = attributes.get('limit');
     const offset = attributes.get('offset');
     if (!offset || !limit) return;
-    console.log("Not returned");
     return {
         limit: limit,
         offset: offset,
@@ -20,7 +17,6 @@ export function formatAttributesToServerPaginationOptions(
 }
 
 export function appendServerPaginationToIri(iri: string, options: ServerPaginationOptions): string {
-    console.log(iri, options);
     const first = iri.includes('?') ? '&' : '?';
     const limit = options.limit;
     const offset = options.offset;
