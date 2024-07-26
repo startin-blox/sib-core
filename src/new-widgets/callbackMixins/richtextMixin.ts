8a308679062f02326381a52272c52da5de3fa47f
Anastasia Kryukova <anastasia.kryukova@protonmail.com>
2024-07-26 14:22:30 +0200

    Bugfix: issue 847 required on rich text does not work

--- src/new-widgets/callbackMixins/richtextMixin.ts
+++ src/new-widgets/callbackMixins/richtextMixin.ts
@@ -33,8 +33,74 @@ const RichtextMixin = {
       const ops = deltaMd.toDelta(this.value);
       this.quill.setContents(ops);
 
+      if (this.isRequired()) {
+        this.createHiddenRequiredInput()
+        this.quill.on('text-change', this.onTextChange.bind(this))
+      }
+
       const nextProcessor = listCallbacks.shift();
       if (nextProcessor) nextProcessor(value, listCallbacks); 
+  },
+  isRequired() {
+    return Array.from(this.element.attributes).some(attr => (attr as Attr).name === 'required')
+  },
+  createHiddenRequiredInput() {
+    const attributeName = this.getAttributeValue('name');
+    this.hiddenInput = document.querySelector(`input[name="${attributeName + '-hidden'}"]`) as HTMLInputElement;
+
+    if (!this.hiddenInput) {
+      this.hiddenInput = this.createHiddenInput(attributeName + '-hidden');
+      this.element.appendChild(this.hiddenInput);
+      this.addInvalidEventListener();
+    }
+    this.hiddenInput.value=this.quill.getText();
+  },
+  createHiddenInput(attributeName: string): HTMLInputElement {
+    const input = document.createElement('input');
+    input.name = attributeName;
+    input.setAttribute('required', 'true');
+    input.style.opacity = '0';
+    input.style.position = 'absolute';
+    input.style.pointerEvents = 'none';
+    return input;
+  },
+  getAttributeValue(attributeName: string): string {
+    const attribute = Array.from(this.element.attributes).find(attr => (attr as Attr).name === attributeName) as Attr;
+    return attribute ? attribute.value : '';
+  },
+  displayCustomErrorMessage(message: string) {
+    const richtext = this.element.querySelector('[data-richtext]'); 
+  
+    if (richtext) {
+      let errorMessageElement = richtext.querySelector('.required-error-message') as HTMLDivElement;
+      
+      if (!errorMessageElement) {
+        errorMessageElement = document.createElement('div');
+        errorMessageElement.className = 'required-error-message';
+      }
+
+      richtext.appendChild(errorMessageElement);
+      errorMessageElement.textContent = message;
+      richtext.classList.add('error-border-richtext');
+    }
+  },
+  addInvalidEventListener() {
+    this.hiddenInput.addEventListener("invalid", (e) => {
+      e.preventDefault();
+      this.displayCustomErrorMessage('Please fill out this field.');
+    });
+  },
+  onTextChange() {
+    this.hiddenInput.value=this.quill.getText();
+    this.removeErrorMessageAndStyling();
+  },
+  removeErrorMessageAndStyling() {
+    // Remove any previously displayed error message and error styling
+    const richtext = this.element.querySelector('[data-richtext]');
+    let errorMessageElement = richtext.querySelector('.required-error-message') as HTMLDivElement;
+    if (errorMessageElement)
+      errorMessageElement.remove()
+    richtext.classList.remove('error-border-richtext');
   },
 }
 
