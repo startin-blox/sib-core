35147be72b80793d1f50f653e6842d1ebcdfca48
Clément <cpartiot@gmail.com>
2025-05-15 09:58:54 +0200

    update: simplify store loaded twice test

--- cypress/e2e/unit/store-loaded-twice.cy.ts
+++ cypress/e2e/unit/store-loaded-twice.cy.ts
@@ -1,39 +1,38 @@
 type StoreLib = typeof import('../../../src/store.ts');
 
+type CustomWin = {
+  loadStore(): Promise<StoreLib>;
+  loadStoreFromCore(): Promise<StoreLib>;
+};
+
 describe('store twice', { testIsolation: false }, () => {
   it('should get the same store data from index.ts and store.ts', () => {
     cy.visit('/examples/e2e/store-loaded-twice.html');
 
-    cy.window().then(async (win: any) => {
-      expect(window.sibStore).to.be.undefined;
-      expect(win.sibStore).to.be.undefined;
-      const { store: storeFromCypress } = (await import(
-        '../../../src/store.ts'
-      )) as StoreLib;
-      expect(window.sibStore).to.not.be.undefined;
+    //@ts-ignore
+    cy.window().then(async (win: Cypress.AUTWindow & CustomWin) => {
       expect(win.sibStore).to.be.undefined;
 
       // import from store.js
-      const { store, baseContext } = (await win.loadStore()) as StoreLib;
-      expect(window.sibStore).to.not.be.undefined;
+      const { store, baseContext } = await win.loadStore();
+
       expect(win.sibStore).to.not.be.undefined;
-      expect(store).to.not.equals(storeFromCypress);
       expect(store).to.equals(win.sibStore);
 
       // import from index.js
-      const { store: storeFromCore } =
-        (await win.loadStoreFromCore()) as StoreLib;
-      expect(storeFromCore).to.equals(store);
-      expect(storeFromCore).to.equals(win.sibStore);
+      const { store: storeCore } = await win.loadStoreFromCore();
+      expect(storeCore).to.equals(store);
+      expect(storeCore).to.equals(win.sibStore);
 
-      expect(storeFromCypress.cache.length()).to.equals(0);
       expect(store.cache.length()).to.equals(0);
-      expect(storeFromCore.cache.length()).to.equals(0);
+      expect(storeCore.cache.length()).to.equals(0);
+      expect(win.sibStore.cache.length()).to.equals(0);
 
       await store.getData('/examples/data/list/user-1.jsonld', baseContext);
-      expect(storeFromCypress.cache.length()).to.equals(0);
+
       expect(store.cache.length()).to.equals(4);
-      expect(storeFromCore.cache.length()).to.equals(4);
+      expect(storeCore.cache.length()).to.equals(4);
+      expect(win.sibStore.cache.length()).to.equals(4);
     });
   });
 });
